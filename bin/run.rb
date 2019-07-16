925c876edb304ebe4ff1835d92534bc2a3de2be1
Harry Chopra <harrychopra@gmail.com>
2019-07-16 21:59:25 +0100

    Completed the lab & also added friendly error messages.

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,9 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
+#Welcome message
 welcome
+#recieves input from the user
 character = get_character_from_user
+#calls the final method in the api_communicator.rb
 show_character_movies(character)
